a3754d54eff73829cfbe81bfc526549b91c5e110
U-soga-pc\soga <soga@soga-pc.(none)>
2012-08-29 19:06:18 +0900

    test001

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 class HelloWorld {
 	
-	public static final int MAX_HELLO = 10;
+	public static final int MAX_HELLO = 20;
 	public static void main(String args[]){
 		System.out.println("Hello World!");
 		for( int idx = 1 ; idx <= MAX_HELLO ; idx++ ){
-			System.out.println("Hello World" + idx);
+			System.out.println( String.format("Hello World %d", idx ));
 		}
 	}
 }
